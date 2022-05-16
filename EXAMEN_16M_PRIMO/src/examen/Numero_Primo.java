0dbaab5b689da8d3d506e6e6fd8b998cd975659a
ALUMNOS <ALUMNOS@P608>
2022-05-16 09:13:04 +0200

    examen

---
+++ EXAMEN_16M_PRIMO/src/examen/Numero_Primo.java
@@ -0,0 +1,10 @@
+package examen;
+
+public class Numero_Primo {
+
+	public static void main(String[] args) {
+
+		System.out.println("Buenos días Ceinmark, soy Ángel Rivas \n" + "Introduce un número para averiguar si es primo o no:");
+	}
+
+}
